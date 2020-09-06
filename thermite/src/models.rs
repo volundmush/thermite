55beedcef418926290e4065ee1133cb1ce084295
Andrew Bastien <volundmush@gmail.com>
2020-09-05 17:56:00 -0700

    Updated Models pre-Sqlite3

--- thermite/src/models.rs
+++ thermite/src/models.rs
@@ -4,28 +4,15 @@ use chrono::{DateTime, TimeZone, NaiveDateTime, Utc};
 pub struct Game {
     pub id: i32,
     pub user_id: i32,
-    pub gamename: String,
+    pub abbr: String,
     pub display_name: String,
     pub created: NaiveDateTime,
-    pub is_public: bool
-}
-
-#[derive(Queryable)]
-pub struct GameApi {
-    pub id: i32,
-    pub game_id: i32,
-    pub game_key: String
-}
-
-#[derive(Queryable)]
-pub struct GameBan {
-    pub id: i32,
-    pub game_id: i32,
-    pub user_id: i32,
-    pub banned_on: NaiveDateTime,
-    pub banned_until: NaiveDateTime,
+    pub active: bool,
+    pub is_public: bool,
+    pub game_key: String,
+    pub banned_until: Option<NaiveDateTime>,
     pub banned_by: Option<i32>,
-    pub ban_reason: String
+    pub ban_reason: Option<String>
 }
 
 #[derive(Queryable)]
@@ -34,7 +21,24 @@ pub struct GameMember {
     pub game_id: i32,
     pub user_id: i32,
     pub joined: NaiveDateTime,
-    pub member_key: String
+    pub member_key: String,
+    pub active: bool,
+    pub is_superuser: bool,
+    pub is_admin: bool,
+    pub banned_until: Option<NaiveDateTime>,
+    pub banned_by: Option<i32>,
+    pub ban_reason: Option<String>
+}
+
+#[derive(Queryable)]
+pub struct MemberStorage {
+    pub id: i32,
+    pub member_id: i32,
+    pub category: String,
+    pub storage_name: String,
+    pub created: NaiveDateTime,
+    pub modified: NaiveDateTime,
+    pub json_data: serde_json::Value
 }
 
 #[derive(Queryable)]
@@ -42,54 +46,18 @@ pub struct User {
     pub id: i32,
     pub username: String,
     pub joined: NaiveDateTime,
-    pub password_hash: Option<String>,
-    pub active: bool,
-    pub is_supervisor: bool
-}
-
-#[derive(Queryable)]
-pub struct UserBan {
-    pub id: i32,
-    pub user_id: i32,
-    pub banned_on: NaiveDateTime,
-    pub banned_until: NaiveDateTime,
-    pub banned_by: Option<i32>,
-    pub ban_reason: String
-}
-
-#[derive(Queryable)]
-pub struct Email {
-    pub id: i32,
-    pub address: String,
-    pub added: NaiveDateTime,
-    pub verified: bool,
-    pub verified_at: Option<NaiveDateTime>
-}
-
-#[derive(Queryable)]
-pub struct UserEmail {
-    pub id: i32,
-    pub user_id: i32,
-    pub email_id: i32,
-    pub added: NaiveDateTime
-}
-
-#[derive(Queryable)]
-pub struct UserPassword {
-    pub id: i32,
-    pub user_id: i32,
     pub password_hash: String,
-    pub added: NaiveDateTime
-}
-
-#[derive(Queryable)]
-pub struct UserProfile {
-    pub id: i32,
-    pub user_id: i32,
-    pub display_name: Option<String>,
-    pub email: Option<i32>,
+    pub email: Option<String>,
+    pub email_verified: bool,
+    pub email_verified_on: Option<NaiveDateTime>,
+    pub active: bool,
+    pub is_superuser: bool,
+    pub is_admin: bool,
     pub lang_tag: String,
-    pub timezone: String
+    pub timezone: String,
+    pub banned_until: Option<NaiveDateTime>,
+    pub banned_by: Option<i32>,
+    pub ban_reason: Option<String>
 }
 
 #[derive(Queryable)]
@@ -110,4 +78,52 @@ pub struct UserStorage {
     pub created: NaiveDateTime,
     pub modified: NaiveDateTime,
     pub json_data: serde_json::Value
+}
+
+#[derive(Queryable)]
+pub struct Board {
+    pub id: i32,
+    pub board_id: i32,
+    pub game_id: Option<i32>,
+    pub name: String,
+    pub display_name: String,
+    pub mandatory: bool,
+    pub restricted: u8,
+    pub next_id: i32
+}
+
+#[derive(Queryable)]
+pub struct Post {
+    pub id: i32,
+    pub board_id: i32,
+    pub post_num: i32,
+    pub subject: String,
+    pub text: String,
+    pub date_created: NaiveDateTime,
+    pub date_modified: NaiveDateTime
+}
+
+#[derive(Queryable)]
+pub struct PostRead {
+    pub id: i32,
+    pub post_id: i32,
+    pub date_checked: NaiveDateTime
+}
+
+#[derive(Queryable)]
+pub struct Channel {
+    pub id: i32,
+    pub game_id: Option<i32>,
+    pub name: String,
+    pub display_name: String,
+    pub restricted: u8
+}
+
+#[derive(Queryable)]
+pub struct ChannelSub {
+    pub id: i32,
+    pub channel_id: i32,
+    pub user_id: i32,
+    pub command: String,
+    pub status: u8
 }
